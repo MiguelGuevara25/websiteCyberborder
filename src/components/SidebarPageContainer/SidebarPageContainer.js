e6fbceddac2907b837e0839b958242d5a02a1777
MiguelGuevara25 <miguel_g1789@hotmail.com>
2024-04-19 20:15:49 -0500

    fixed partners

--- src/components/SidebarPageContainer/SidebarPageContainer.js
+++ src/components/SidebarPageContainer/SidebarPageContainer.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { Col, Row } from "react-bootstrap";
 import ContentSide from "./ContentSide";
 import SidebarSide from "./SidebarSide";
--- src/pages/partners/leghorn-group.js
+++ src/pages/partners/leghorn-group.js
@@ -1,4 +1,3 @@
-import PageBanner from "@/components/BannerSection/PageBanner";
 import HeaderOne from "@/components/Header/HeaderOne";
 import MobileMenu from "@/components/Header/MobileMenu";
 import Layout from "@/components/Layout/Layout";
